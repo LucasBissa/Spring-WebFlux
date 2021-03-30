eef9ec1f437af045cb403fc85c21d2d44af042f9
LucasBissa <lucasbomfimddt@gmail.com>
2021-03-30 18:40:10 -0300

    remove getters and setters

--- src/main/java/com/api/document/Heroes.java
+++ src/main/java/com/api/document/Heroes.java
@@ -12,37 +12,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @DynamoDBTable(tableName ="Heroes_Api_Table")
 public class Heroes {
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getUniverse() {
-    return universe;
-  }
-
-  public void setUniverse(String universe) {
-    this.universe = universe;
-  }
-
-  public int getFilms() {
-    return films;
-  }
-
-  public void setFilms(int films) {
-    this.films = films;
-  }
 
   @Id
   @DynamoDBHashKey (attributeName = "id")
